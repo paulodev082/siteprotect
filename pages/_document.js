19e0f32f265676fc89137d061d6d5cc623508aea
Paulo Dev <paulotocadoguerreiro.26@gmail.com>
2025-07-19 13:26:35 -0300

    Corrigir uso do <Html> e mover para _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        {/* Ícone principal do site */}
+        {/* ícone principal do site */}
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="theme-color" content="#2563EB" />
 
-        {/* Ícones opcionais para dispositivos Apple/Android */}
+        {/* ícones opcionais para dispositivos Apple/Android */}
         {/*
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
